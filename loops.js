2fe7fcfa484abf844b1900bdc596796589e33c0e
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-10 23:32:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 
 function forLoop(array){
+<<<<<<< HEAD
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
     array.push("I am 1 strange loop." )
@@ -8,6 +9,16 @@ function forLoop(array){
   }
   }
   return array
+=======
+  for (i = 0; i < 24; i++) {
+    if (i === 1) {
+    array.push("I am " + i + " strange loop." )
+  } else if (i != 1){
+    array.push(`I am ${i} strange loops.`)
+  }
+   return array
+  }
+>>>>>>> b9d21bf12685d2424f575477ced4959e786d9115
 }
 
 function whileLoop(n){
